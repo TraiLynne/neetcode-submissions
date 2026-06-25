bc778ca0c6468b771f1de6436ea3455e32fbd5bb
Trai Lynne Compton <trailynne@gmail.com>
2026-06-25 09:13:38 -0600

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        if (nums.length < 2) return false;
+
+        let visited = {};
+
+        for(let i = 0; i < nums.length; i++){
+            if(visited[nums[i]]){ 
+                return true
+            } else {
+                visited[nums[i]] = 1
+            }
+        }
+
+        return false
+    }
+}
